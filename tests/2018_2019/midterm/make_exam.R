d66d70f436e8da2f4cd52dcc910de6eeec0c2a1c
Boris Demeshev <boris.demeshev@gmail.com>
2019-06-12 13:29:29 +0300

    2016_2017 ressurection

--- tests/2018_2019/midterm/make_exam.R
+++ tests/2018_2019/midterm/make_exam.R
@@ -1,78 +1,15 @@
-# install most recent version of exam package
 # install.packages("exams", repos="http://R-Forge.R-project.org")
-
 library(exams)
 
-# check whether latex is present and specify path to latexmk
-Sys.which("latexmk")
+Sys.which("latexmk") # check whether latex is present and specify path to latexmk
 latex_executable <- Sys.which("pdflatex")
 options(texi2dvi = latex_executable)
 
-
-
 files_all <- list.files('rmd/', pattern = "*.Rmd", full.names = TRUE)
 files_all
-
-
-
-
-# every variant should go into separate folder
-# for the moment n > 1 does overwrite old questions in texdir folder
-# scrambling is done by sample()
-
-
-
-# set.seed(777)
-# files_shuffled <- sample(files_all)
-
-
-useful = setdiff(2*(1:34), c(7, 8, 35, 36, 43, 48, 10))
-
+useful = setdiff(2 * (1:34), c(7, 8, 35, 36, 43, 48, 10))
 files_sample = files_all[useful]
-
-
-# dir.create("pdf")
-# dir.create("tex")
-
-# exams <- exams2nops(files_sample, n = 1, startid = 665 +
-#                     dir = "pdf",
-#                     verbose = TRUE,
-#                     language = "ru",
-#             texdir = "tex",
-#             name = "exam_",
-#             date = "2018-09-18", institution = "Высшая школа экономики",
-#             logo = "",
-#             encoding = "UTF-8",
-#             samepage = TRUE,
-#             reglength = 3, # is not working?
-#             blank = 0,
-#             header = "\\input{../header.tex}",
-#             title = "Теория вероятностей :)")
-# 
-# sol <- exams_metainfo(exams)
-# print(sol, 1)
-
-
-# 
-# exams <- exams2pdf(files_sample, n = 1,
-#                     dir = "pdf",
-#                     verbose = TRUE,
-#                     language = "ru",
-#             texdir = "tex",
-#             name = "the_exam",
-#             date = "2018-09-18", institution = "Higher School of Economics",
-#             logo = "",
-#             encoding = "UTF-8",
-#             samepage = TRUE,
-#             reglength = 3, # is not working?
-#             blank = 0,
-#             header = "\\input{../header.tex}",
-#             title = "Probability theory and Statistics")
-# 
-
-
-
-
+files_sample
 
 exams2pdf_source = function(files_sample, n_vars = 1, add_seed = 777, 
                             pdf_dir = "pdf",
